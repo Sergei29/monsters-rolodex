0414194765507e4e13d2b5a2b92d1b01175b9d2c
Sergejs Basangovs <sergejs.basangovs@ingresso.co.uk>
2020-05-15 11:42:25 +0100

    Updates

---
+++ precache-manifest.7bdb64c4db29281028fb35fb04147c0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835d7de805ec70731ef9a4ce6af60e7d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5d75ee6be46355b6dee6",
+    "url": "/monsters-rolodex/static/css/main.9b24980f.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d75ee6be46355b6dee6",
+    "url": "/monsters-rolodex/static/js/main.543d54fe.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
